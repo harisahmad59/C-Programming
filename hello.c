29cc6a29ddbd9656844e84ed575f218bda29594d
Haris Ahmad <119689990+harisahmad59@users.noreply.github.com>
2023-08-22 18:26:59 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { 
+int x,y; 
+printf("enter x");
+scanf("%d", &x);
+
+printf("enter y");
+scanf("%d", &y);
+
+int sum =  x + y;
+printf("sum of x and y is %d", sum);
+  return 0;
+ }
